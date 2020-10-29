9a7be2aa26b01dd7a5136b56e2441ff1eff91dc0
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-10-29 22:37:14 +0200

    Add products and Product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <g-link v-if="prefix.cool" :to="prefix.path">
+        <h1>{{ prefix.title }}</h1>
+        <h2>Date added is: {{ prefix.date_added }}</h2>
+        <h3>ID is: {{ prefix.prodID }}</h3>
+        <p>{{ prefix.description }}</p>
+    </g-link>
+</template>
+
+<script>
+export default {
+    props: ["prefix"],
+};
+</script>
---
+++ src/pages/Products.vue
@@ -0,0 +1,39 @@
+<template>
+    <Layout>
+        <ProductCard
+            v-for="edge in $page.products.edges"
+            :key="edge.node.id"
+            :prefix="edge.node"
+        />
+    </Layout>
+</template>
+
+<script>
+import ProductCard from "../components/ProductCard.vue";
+export default {
+    components: {
+        ProductCard,
+    },
+    metaInfo: {
+        title: "Products",
+    },
+};
+</script>
+
+<page-query>
+query Products {
+  products: allProduct {
+    edges {
+      node {
+        id
+        path
+        title
+        prodID
+        date_added (format:"MMM DD, YYYY")
+        description
+        cool
+      }
+    }
+  }
+}
+</page-query>
