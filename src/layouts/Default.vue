500283b744bf160b2b01d676141531cb2b0d5d0a
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-10-31 12:52:22 +0200

    Chnage default content

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="layout">
+  <div id="app" class="wrapper">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
+      <div class="header__left">
+        <Logo v-if="showLogo" />
+      </div>
+      <div class="header__right">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/posts/">Posts</g-link>
         <g-link class="nav__link" to="/reviews/">Reviews</g-link>
-      </nav>
+      </div>
     </header>
-    <slot/>
+    <main class="main">
+      <slot />
+    </main>
+    <footer class="footer">
+      <p class="footer__links">
+        <a href="//twitter.com/yonatankof" target="_blank">Twitter</a>
+        <a href="//github.com/yonatankof" target="_blank">GitHub</a>
+        <a href="//linkedin.com/in/yonatankof/" target="_blank">Linkedin</a>
+        <a href="//dribbble.com/yonatan_kof" target="_blank">Dribbble</a>
+        <ToggleTheme />
+      </p>
+      <span class="footer__copyright">
+        Copyright © {{ $static.metadata.siteName }}・{{
+          new Date().getFullYear()
+        }}
+      </span>
+    </footer>
   </div>
 </template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import ToggleTheme from "~/components/ToggleTheme.vue";
+
+export default {
+  props: {
+    showLogo: { default: true },
+    isSelected: { default: false },
+    showPosts: { default: true },
+  },
+  components: {
+    Logo,
+    ToggleTheme,
+  },
+};
+</script>
 
 <static-query>
 query {
@@ -22,30 +56,64 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+<style lang="scss">
+.wrapper {
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
 }
-
-.layout {
-  max-width: 760px;
+.main {
   margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding: 0 var(--content-space);
 }
-
 .header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  min-height: var(--header-height);
+  padding: 0 var(--content-space);
+  top: 0;
+  z-index: 10;
+  .active--exact {
+    border-bottom: 1px solid var(--link-color);
+  }
+  &__left,
+  a {
+    margin: 0;
+    margin-inline-end: var(--space-md);
+  }
+  &__right {
+    a {
+      margin: 0;
+      margin-inline-start: var(--space-md);
+    }
+  }
+  @media screen and (min-width: 1300px) {
+    //Make header sticky for large screens
+    position: sticky;
+    max-width: 100%;
+  }
 }
 
-.nav__link {
-  margin-left: 20px;
+.footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: var(--content-space);
+  margin-bottom: var(--spacem-md);
+  text-align: center;
+  &__links {
+    display: flex;
+    align-items: center;
+    a {
+      margin: 0;
+      margin-inline-end: var(--spacem-sm);
+    }
+  }
+  > span {
+    font-size: 0.8em;
+    margin-top: var(--spacem-xs) 0;
+  }
 }
 </style>
