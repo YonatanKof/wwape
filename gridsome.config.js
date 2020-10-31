f0ed41399d76f4f6feb7fb31726c9bd70291c188
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-10-31 12:16:08 +0200

    Structure collections

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,9 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: "Gridsome",
+    siteName: "Kof'R'Us",
+    siteDescription:
+        "Yonatan Ben Knaan - A design system manager, a creative director, a mismatched persona and an alright dude.",
     plugins: [
         {
             use: "@gridsome/vue-remark",
@@ -15,6 +17,28 @@ module.exports = {
                 pathPrefix: "/review", // Add route prefix. Optional
                 template: "./src/templates/Review.vue", // Optional
                 plugins: ["@gridsome/remark-prismjs"],
+            },
+        },
+        {
+            use: "@gridsome/vue-remark",
+            options: {
+                typeName: "Tag",
+                baseDir: "./content/tags",
+                pathPrefix: "/tag",
+                template: "./src/templates/Tag.vue",
+            },
+        },
+        {
+            use: "@gridsome/vue-remark",
+            options: {
+                typeName: "Post", // Required
+                baseDir: "./content/posts", // Where .md files are located
+                pathPrefix: "/post", // Add route prefix. Optional
+                template: "./src/templates/Post.vue", // Optional
+                plugins: ["@gridsome/remark-prismjs"],
+                refs: {
+                    tags: "Tag",
+                },
             },
         },
     ],
