5a8921934d03a1c6bd01e15cfb66178eb8a45668
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-11-02 20:25:58 +0200

    Add safe-area-inset CSS

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,33 +1,37 @@
 <template>
-  <div id="app" class="wrapper">
-    <header class="header">
-      <div class="header__left">
-        <Logo v-if="showLogo" />
-      </div>
-      <div class="header__right">
-        <g-link class="nav__link" to="/about/">About</g-link>
-        <g-link class="nav__link" to="/posts/">Posts</g-link>
-        <g-link class="nav__link" to="/reviews/">Reviews</g-link>
-      </div>
-    </header>
-    <main class="main">
-      <slot />
-    </main>
-    <footer class="footer">
-      <p class="footer__links">
-        <a href="//twitter.com/yonatankof" target="_blank">Twitter</a>
-        <a href="//github.com/yonatankof" target="_blank">GitHub</a>
-        <a href="//linkedin.com/in/yonatankof/" target="_blank">Linkedin</a>
-        <a href="//dribbble.com/yonatan_kof" target="_blank">Dribbble</a>
-        <ToggleTheme />
-      </p>
-      <span class="footer__copyright">
-        Copyright © {{ $static.metadata.siteName }}・{{
-          new Date().getFullYear()
-        }}
-      </span>
-    </footer>
-  </div>
+    <div id="app" class="wrapper">
+        <header class="header">
+            <div class="header__left">
+                <Logo v-if="showLogo" />
+            </div>
+            <div class="header__right">
+                <g-link class="nav__link" to="/about/">About</g-link>
+                <g-link class="nav__link" to="/posts/">Posts</g-link>
+                <g-link class="nav__link" to="/reviews/">Reviews</g-link>
+            </div>
+        </header>
+        <main class="main">
+            <slot />
+        </main>
+        <footer class="footer">
+            <p class="footer__links">
+                <a href="//twitter.com/yonatankof" target="_blank">Twitter</a>
+                <a href="//github.com/yonatankof" target="_blank">GitHub</a>
+                <a href="//linkedin.com/in/yonatankof/" target="_blank"
+                    >Linkedin</a
+                >
+                <a href="//dribbble.com/yonatan_kof" target="_blank"
+                    >Dribbble</a
+                >
+                <ToggleTheme />
+            </p>
+            <span class="footer__copyright">
+                Copyright © {{ $static.metadata.siteName }}・{{
+                    new Date().getFullYear()
+                }}
+            </span>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -35,15 +39,15 @@ import Logo from "~/components/Logo.vue";
 import ToggleTheme from "~/components/ToggleTheme.vue";
 
 export default {
-  props: {
-    showLogo: { default: true },
-    isSelected: { default: false },
-    showPosts: { default: true },
-  },
-  components: {
-    Logo,
-    ToggleTheme,
-  },
+    props: {
+        showLogo: { default: true },
+        isSelected: { default: false },
+        showPosts: { default: true },
+    },
+    components: {
+        Logo,
+        ToggleTheme,
+    },
 };
 </script>
 
@@ -57,62 +61,64 @@ query {
 
 <style lang="scss">
 .wrapper {
-  min-height: 100vh;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr auto;
 }
 .main {
-  margin: 0 auto;
-  padding: 0 var(--content-space);
+    margin: 0 auto;
+    padding: 0;
+    padding-left: max(var(--content-space), env(safe-area-inset-left));
+    padding-right: max(var(--content-space), env(safe-area-inset-right));
 }
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  min-height: var(--header-height);
-  padding: 0 var(--content-space);
-  top: 0;
-  z-index: 10;
-  .active--exact {
-    border-bottom: 1px solid var(--link-color);
-  }
-  &__left,
-  a {
-    margin: 0;
-    margin-inline-end: var(--space-md);
-  }
-  &__right {
-    a {
-      margin: 0;
-      margin-inline-start: var(--space-md);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-height: var(--header-height);
+    padding: 0 var(--content-space);
+    top: 0;
+    z-index: 10;
+    .active--exact {
+        border-bottom: 1px solid var(--link-color);
     }
-  }
-  @media screen and (min-width: 1300px) {
-    //Make header sticky for large screens
-    position: sticky;
-    max-width: 100%;
-  }
+    &__left,
+    a {
+        margin: 0;
+        margin-inline-end: var(--space-md);
+    }
+    &__right {
+        a {
+            margin: 0;
+            margin-inline-start: var(--space-md);
+        }
+    }
+    @media screen and (min-width: 1300px) {
+        //Make header sticky for large screens
+        position: sticky;
+        max-width: 100%;
+    }
 }
 
 .footer {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: var(--content-space);
-  margin-bottom: var(--spacem-md);
-  text-align: center;
-  &__links {
     display: flex;
+    flex-direction: column;
     align-items: center;
-    a {
-      margin: 0;
-      margin-inline-end: var(--spacem-sm);
+    justify-content: center;
+    padding: var(--content-space);
+    margin-bottom: var(--spacem-md);
+    text-align: center;
+    &__links {
+        display: flex;
+        align-items: center;
+        a {
+            margin: 0;
+            margin-inline-end: var(--spacem-sm);
+        }
     }
-  }
-  > span {
-    font-size: 0.8em;
-    margin-top: var(--spacem-xs) 0;
-  }
+    > span {
+        font-size: 0.8em;
+        margin-top: var(--spacem-xs) 0;
+    }
 }
 </style>
