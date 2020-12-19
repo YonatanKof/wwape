2dbf299dfb0ffc7f2025ea587311f7912d0a21a7
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2020-12-19 21:02:20 +0200

    Good looking comp

--- src/components/forPosts/LShape.vue
+++ src/components/forPosts/LShape.vue
@@ -3,10 +3,15 @@
         <div
             class="l-shape"
             v-bind:style="[
-                { perspective: compPres + 'em' },
                 {
                     transform:
-                        'rotateX(' +
+                        'scale(' +
+                        compScale +
+                        '%) perspective(' +
+                        compPres +
+                        'em) translateY(' +
+                        compAxleY +
+                        '%) rotateX(' +
                         compRotX +
                         'deg) rotateY(' +
                         compRotY +
@@ -26,50 +31,73 @@
             <div class="face-me plane plane-top-1">Plane Top 1</div>
             <div class="face-me plane plane-top-2">Plane Top 2</div>
             <div class="face-me plane plane-bottom">Plane Bottom</div>
+            <div class="face-me plane plane-bottom shadow"></div>
         </div>
         <input
-            class="this-input"
-            type="range"
             name="perspective"
-            min="50"
+            type="range"
+            class="this-input"
+            min="30"
             max="300"
             v-bind:value="compPres"
             data-sizing="em"
             orient="vertical"
             v-on:input="onCompPres"
         />
-        <input
-            class="this-input"
-            type="range"
+        <!-- <input
             name="rot-x"
+            type="range"
+            class="this-input"
             min="-360"
             max="360"
             v-bind:value="compRotX"
             data-sizing="deg"
             orient="vertical"
             v-on:input="onCompRotX"
-        />
+        /> -->
         <input
-            class="this-input"
+            name="scale"
             type="range"
+            class="this-input"
+            min="60"
+            max="100"
+            v-bind:value="compScale"
+            data-sizing="deg"
+            orient="vertical"
+            v-on:input="onCompScale"
+        />
+        <!-- <input
             name="rot-y"
+            type="range"
+            class="this-input"
             min="-360"
             max="360"
             v-bind:value="compRotY"
             data-sizing="deg"
             orient="vertical"
             v-on:input="onCompRotY"
-        />
-        <input
-            class="this-input"
-            type="range"
+        /> -->
+        <!-- <input
             name="rot-z"
+            type="range"
+            class="this-input"
             min="-360"
             max="360"
             v-bind:value="compRotZ"
             data-sizing="deg"
             orient="vertical"
             v-on:input="onCompRotZ"
+        /> -->
+        <input
+            name="axel-y"
+            type="range"
+            class="this-input"
+            min="-25"
+            max="25"
+            v-bind:value="compAxleY"
+            data-sizing="%"
+            orient="vertical"
+            v-on:input="onCompAxleY"
         />
     </div>
 </template>
@@ -78,10 +106,12 @@
 export default {
     data() {
         return {
-            compPres: 300,
+            compPres: 90,
             compRotX: 0,
-            compRotY: 0,
+            compRotY: 320,
             compRotZ: 0,
+            compScale: 80,
+            compAxleY: 8, 
         };
     },
     methods: {
@@ -96,6 +126,12 @@ export default {
         },
         onCompRotZ(e) {
             this.compRotZ = e.target.value;
+        },
+        onCompAxleY(e) {
+            this.compAxleY = e.target.value;
+        },
+        onCompScale(e) {
+            this.compScale = e.target.value;
         },
     },
 };
@@ -114,27 +150,29 @@ export default {
     display: flex;
 }
 .l-shape {
-    transition: all 0.35s;
+    // transition: all 0.35s;
     font-size: 12px;
     width: 100%;
     height: 100%;
     backface-visibility: visible;
-    perspective-origin: 150% 150%;
+    perspective-origin: 50% 50%;
     transform-style: preserve-3d;
     // perspective: 1000em;
     // transform: rotateX(-30deg) rotateY(-30deg);
+    
 }
 .face-me {
     display: block;
     position: absolute;
-    border: var(--system-color) solid 0.1em;
+    // border: var(--system-color) solid 0.1em;
     border-radius: 0.25em;
     font-family: sans-serif;
     color: white;
     text-align: center;
 }
 .side {
-    background: var(--border-color);
+    // background: var(--border-color);
+    background: burlywood;
 }
 .side-1a {
     width: 10em;
@@ -149,19 +187,21 @@ export default {
     transform: translateX(10em) translateY(20em) translateZ(5em);
 }
 .side-2a {
+    background: chocolate;
     width: 10em;
     height: 30em;
     line-height: 30em;
     transform: rotateY(180deg) translateZ(5em);
 }
 .side-2b {
+    background: chocolate;
     width: 10em;
     height: 10em;
     line-height: 10em;
     transform: translateX(10em) rotateY(180deg) translateY(20em) translateZ(5em);
 }
 .front {
-    background: var(--border-color);
+    background: salmon;
 }
 .front-1 {
     width: 10em;
@@ -183,7 +223,7 @@ export default {
     transform: rotateY(-90deg) translateZ(5em);
 }
 .plane {
-    background: var(--border-color);
+    background: firebrick;
 }
 .plane-top-1 {
     width: 10em;
@@ -202,5 +242,13 @@ export default {
     height: 10em;
     line-height: 10em;
     transform: translateY(20em) rotateX(-90deg) translateZ(5em);
+}
+.shadow {
+    width: 20em;
+    height: 10em;
+    transform: translateY(20.5em) rotateX(-90deg) translateZ(5em);
+    background-color: rgba(black, 0.5);
+    border-radius: 10em;
+    filter: blur(4em);
 }
 </style>
