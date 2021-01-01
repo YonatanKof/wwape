debdc6bac5f3d52064ef98be9f01a477810faff4
Yonatan Ben-Knaan <yonatan.bk@pepperi.com>
2021-01-01 21:22:30 +0200

    See list
    - Full width on small screens
    - Remove user agent margin from input
    - Add SVG base gridline
    - Transform for Gridline
    - Better labels
    - Design tweaks

--- src/components/forPosts/LShape.vue
+++ src/components/forPosts/LShape.vue
@@ -29,6 +29,17 @@
                 <div class="face-me plane plane-top-2">Plane Top</div>
                 <div class="face-me plane plane-bottom">Plane Bottom</div>
                 <div class="face-me plane plane-bottom shadow"></div>
+                <GridLine
+                    class="face-me grid-line"
+                    v-bind:style="[
+                        {
+                            transform:
+                                'scaleX(2) scaleZ(2) rotateX(90deg) translateX(-2.5em) translateZ(' +
+                                compGridLineY +
+                                'em)',
+                        },
+                    ]"
+                />
             </div>
         </div>
         <fieldset class="pers-form">
@@ -36,12 +47,12 @@
                 v-if="showCompPres"
                 class="pers-form__label"
                 for="perspective"
-                >Perspective
+                >Station Point
                 <input
                     class="pers-form__input"
                     id="perspective"
                     type="range"
-                    min="30"
+                    min="40"
                     max="300"
                     v-bind:value="compPres"
                     v-on:input="onCompPres"
@@ -49,12 +60,12 @@
             </label>
 
             <label v-if="showCompScale" class="pers-form__label" for="scale"
-                >Distance
+                >Size
                 <input
                     class="pers-form__input"
                     id="scale"
                     type="range"
-                    min=".5"
+                    min=".25"
                     max="1.5"
                     step="0.01"
                     v-bind:value="compScale"
@@ -62,7 +73,7 @@
                 />
             </label>
             <label v-if="showCompRotY" class="pers-form__label" for="rot-y"
-                >Rotate Y
+                >Rotation
                 <input
                     class="pers-form__input"
                     id="rot-y"
@@ -74,15 +85,31 @@
                 />
             </label>
             <label v-if="showCompAxleY" class="pers-form__label" for="axel-y"
-                >Height
+                >Horizon Plane (Viewer Height)
                 <input
                     class="pers-form__input"
                     id="axel-y"
                     type="range"
-                    min="-20"
+                    min="-5"
                     max="40"
                     v-bind:value="compAxleY"
                     v-on:input="onCompAxleY"
+                />
+            </label>
+            <label
+                v-if="showGridLineY"
+                class="pers-form__label"
+                for="grid-line-y"
+                >Ground Plane
+                <input
+                    class="pers-form__input"
+                    id="grid-line-y"
+                    type="range"
+                    min="-5"
+                    max="25"
+                    step="1"
+                    v-bind:value="compGridLineY"
+                    v-on:input="onCompGridLineY"
                 />
             </label>
         </fieldset>
@@ -90,14 +117,26 @@
 </template>
 
 <script>
+import GridLine from "../forPosts/GridLine.vue";
+
 export default {
-    props: ["showCompPres", "showCompScale", "showCompRotY", "showCompAxleY"],
+    components: {
+        GridLine,
+    },
+    props: [
+        "showCompPres",
+        "showCompScale",
+        "showCompRotY",
+        "showCompAxleY",
+        "showGridLineY",
+    ],
     data() {
         return {
-            compPres: 40,
+            compPres: 55,
             compRotY: 320,
             compScale: 1,
             compAxleY: 6,
+            compGridLineY: -5,
         };
     },
     methods: {
@@ -113,26 +152,38 @@ export default {
         onCompScale(e) {
             this.compScale = e.target.value;
         },
+        onCompGridLineY(e) {
+            this.compGridLineY = e.target.value;
+        },
     },
 };
 </script>
 
 <style lang="scss">
 @import "../../assets/style/_colors.scss";
+@import "../../assets/style/_layout.scss";
 
 $color-l-shape: hsl(35, 85%, 55%);
 
 .three-d-group {
     z-index: 0;
     width: 100%;
-    height: 48rem;
+    height: 40rem;
     background-color: var(--bg-primary);
     overflow: hidden;
     border-radius: var(--radius);
     display: flex;
+    @include mQ-max($display-size-sm) {
+        margin-inline-start: calc(var(--content-space) * -1);
+        margin-inline-end: calc(var(--content-space) * -1);
+        max-width: unset;
+        width: calc(100% + 4rem);
+        border-radius: 0;
+    }
+    margin-block-end: var(--space-md);
 }
-.l-shape-cont{
-    width: 50%;
+.l-shape-cont {
+    width: 60%;
     padding: 2rem;
     padding-inline-end: 1rem;
 }
@@ -142,7 +193,6 @@ $color-l-shape: hsl(35, 85%, 55%);
     width: 100%;
     height: 100%;
     backface-visibility: visible;
-    // perspective-origin: 50% 50%;
     transform-style: preserve-3d;
 }
 .face-me {
@@ -273,20 +323,34 @@ $color-l-shape: hsl(35, 85%, 55%);
     border-radius: 10em;
     filter: blur(4em);
 }
+.grid-line {
+    background-color: var(--bg-pre);
+    // transform: rotateX(90deg) translateX(-6em) translateZ(-5em);
+    g {
+        stroke: var(--border-color);
+    }
+}
 .pers-form {
-    width: 50%;
+    width: 40%;
+    min-width: 16rem;
     z-index: 100;
     padding: 2rem;
-    padding-inline-start: 1rem;
     display: flex;
     flex-direction: column;
+    background-color: var(--bg-code-invert);
+    box-shadow: var(--shadow-md);
     &__label {
+        font-family: var(--font-family--hand);
+        font-weight: var(--font-wight--bolder);
+        font-size: var(--font-size-2xl);
         padding: 1rem 0;
         text-align: start;
         display: inline-block;
     }
     &__input {
+        margin: 0;
         padding: 1rem 0;
+        width: 100%;
     }
 }
 </style>
