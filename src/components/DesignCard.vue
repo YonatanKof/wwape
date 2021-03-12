b3d1e7337681baf87c4dd253991f0c4f09679880
Yonatan Kof <[kof@yonatankof.com]>
2021-03-12 16:51:16 +0200

    Better design card

--- src/components/DesignCard.vue
+++ src/components/DesignCard.vue
@@ -1,12 +1,17 @@
 <template>
-    <article>
+    <article class="design-card">
+        <h2 class="design-card__title">{{ designData.title }}</h2>
         <g-image
-            alt="Cover image"
+            :alt="designData.cover_caption"
             v-if="designData.cover_image"
-            class=""
+            class="-content-box-image"
             :src="designData.cover_image"
+            fit="contain"
+            position="left bottom"
+            background="red"
+            width="100"
         />
-        <g-link class="" :to="designData.path">Go to {{ designData.title }}</g-link>
+        <g-link class="design-card__link" :to="designData.path"> </g-link>
     </article>
 </template>
 
@@ -15,3 +20,21 @@ export default {
     props: ["designData"],
 };
 </script>
+
+<style lang="scss">
+@import "../assets/style/_content-box.scss";
+@import "../assets/style/_shadow.scss";
+
+.design-card {
+    @include content-box($display-size-xs);
+    @include content-box-hover;    
+    &__titlebackground {
+        text-align: center;
+        margin: var(--space-sm);
+        font-size: var(--font-size-lg);
+    }
+    &__link {
+        @include content-box-Link;
+    }
+}
+</style>
