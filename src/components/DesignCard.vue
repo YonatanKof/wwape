911adb2494d31483a3546c2d0d62989a6a43a401
Yonatan Kof <[kof@yonatankof.com]>
2021-05-17 20:47:07 +0300

    Add avoid break-inside

--- src/components/DesignCard.vue
+++ src/components/DesignCard.vue
@@ -1,23 +1,23 @@
 <template>
-    <article class="design-card">
-        <h2 class="-title">{{ designData.title }}</h2>
-        <g-image
-            :alt="designData.cover_caption"
-            v-if="designData.cover_image"
-            class="-image"
-            :src="designData.cover_image"
-            fit="contain"
-            position="left bottom"
-            background="red"
-            width="100"
-        />
-        <g-link class="-link" :to="designData.path"> </g-link>
-    </article>
+	<article class="design-card">
+		<h2 class="-title">{{ designData.title }}</h2>
+		<g-image
+			:alt="designData.cover_caption"
+			v-if="designData.cover_image"
+			class="-image"
+			:src="designData.cover_image"
+			fit="contain"
+			position="left bottom"
+			background="red"
+			width="100"
+		/>
+		<g-link class="-link" :to="designData.path"> </g-link>
+	</article>
 </template>
 
 <script>
 export default {
-    props: ["designData"],
+	props: ["designData"],
 };
 </script>
 
@@ -26,13 +26,16 @@ export default {
 @import "../assets/style/_shadow.scss";
 
 .design-card {
-    @include content-box($display-size-xs);
-    @include content-box-Hover;
-    > .-title {
-        text-align: center;
-        margin: var(--space-md);
-        margin-top: var(--space-xl);
-        font-size: var(--font-size-lg);
-    }
+	@include content-box($display-size-xs);
+	@include content-box-Hover;
+    margin-block-end: var(--content-space);
+    -webkit-column-break-inside: avoid;
+	break-inside: avoid;
+	> .-title {
+		text-align: center;
+		margin: var(--space-md);
+		margin-top: var(--space-xl);
+		font-size: var(--font-size-lg);
+	}
 }
 </style>
