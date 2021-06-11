099656e61eab49b33d4724dcc1320cd34a2eed37
Yonatan Kof <[kof@yonatankof.com]>
2021-06-11 21:36:24 +0300

    Add rotating logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,27 @@
 <template>
-    <g-link to="/">Index</g-link>
+	<div>
+		<g-link to="/" style="height:40px;">
+			<IconBase icon-name="Back to homepage" width="40" height="40" viewBox="0 0 298 298">
+				<KofIconRotating />
+			</IconBase>
+		</g-link>
+	</div>
 </template>
 
+<script>
+import IconBase from "~/components/IconBase";
+import KofIconRotating from "~/components/icons/KofIconRotating";
 
-<style lang="scss">
+export default {
+	components: {
+		IconBase,
+		KofIconRotating,
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+	border-bottom: 0;
+}
 </style>
