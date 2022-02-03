44b389d70c165c56a24f148d3225e0cdf6cb8a49
Yonatan Kof <kof@yonatankof.com>
2022-02-03 13:13:51 +0200

    Remove scrollanimation

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@
 // Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import "~/assets/style/index.scss";
-import ScrollAnimation from "./directives/scrollanimation"
 import DefaultLayout from "~/layouts/Default.vue";
 
 
 export default function (Vue, { router, head, isClient }) {
     // Set default layout as a global component
     Vue.component("Layout", DefaultLayout);
-    Vue.directive("scrollanimation", ScrollAnimation);
     head.script.push({
         async: true,
         src: "https://cdn.splitbee.io/sb.js",
